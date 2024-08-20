b376bf71c6fc619a55597a50550fd6c9c82d514d
medoo0098 <m@hdi.cool>
2024-08-20 18:34:13 +0100

    refactor main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from config import app, db
-from flask import render_template
 
+from routes import *
 with app.app_context():
     db.create_all()
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
 app.run(debug=True, host="0.0.0.0", port=5001)
