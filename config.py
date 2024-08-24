6b04928fce7dab2eae040a3e54e2eebb0f9f9749
medoo0098 <m@hdi.cool>
2024-08-24 09:18:37 +0100

    config updated

--- config.py
+++ config.py
@@ -1,19 +1,22 @@
 import os
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 
 
 # import enviromental variables 
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-DATABASE_URL = os.getenv("DATABASE_URL")
+load_dotenv()  # makking .env file easilly accessible for the fucntion 
 
 
-class Config:
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
-    SQLAlchemy_TRACK_MODIFICATIONS = True
+class Config:  
+    """
+    configuring the app parameters 
+    """
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # gets the database url from specified location in here its from .env as a enviroment variable
+    SQLAlchemy_TRACK_MODIFICATIONS = True  # when changes are done in DB , chnages will apear in the terminal
     DEBUG = True
-    SECRET_KEY = SECRET_KEY
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+db = SQLAlchemy()
 
 
 
