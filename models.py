bbccef24591bf42b41c55f9f0a00350b4780cc34
medoo0098 <m@hdi.cool>
2024-08-26 00:34:26 +0100

    user model was edited to acomodate user login fiwth flask

--- models.py
+++ models.py
@@ -1,9 +1,13 @@
 from config import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80) , unique=False, nullable=False)
+    password = db.Column(db.String(512) , unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
 
 
 class ToDo(db.Model):
